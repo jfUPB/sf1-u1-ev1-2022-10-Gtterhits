f0066140b09a45866ac424c59e469e5308941fe5
SEBAS <lariag94hd@gmail.com>
2022-02-25 20:26:45 -0500

    9

--- bomb/bomb.ino
+++ bomb/bomb.ino
@@ -10,7 +10,7 @@ bool evBtns = false;
 uint8_t evBtnsData = 0;
 
 void btnsTask();
-void task();
+void pricipalTask();
 
 // Selecciona uno según tu display.
 //SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_128_32);
@@ -18,13 +18,13 @@ SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_64_48);
 
 void setup() {
   btnsTask();
-  task();
+  pricipalTask();
   Serial.begin(115200);
 }
 
 void loop() {
   btnsTask();
-  task();
+  pricipalTask();
 }
 
 void btnsTask() {
@@ -129,13 +129,10 @@ void btnsTask() {
 }
 
 
-void task() {
+void pricipalTask() {
   enum class BombStates {INIT, CONFIG, ARMED};
   static BombStates bombState =  BombStates::INIT;
   static uint8_t counter = 20;
-
-
-
 
   switch (bombState) {
 
@@ -170,6 +167,10 @@ void task() {
 
     case BombStates::CONFIG: {
 
+        display.clear();
+        display.drawString(10, 20, String(counter));
+        display.display();
+
         if (evBtns == true) {
           evBtns = false;
 
@@ -177,17 +178,12 @@ void task() {
             if (counter < 60) {
               counter++;
             }
-            display.clear();
-            display.drawString(10, 20, String(counter));
-            display.display();
+
           }
           else if (evBtnsData == DOWN_BTN) {
             if (counter > 10) {
               counter--;
             }
-            display.clear();
-            display.drawString(10, 20, String(counter));
-            display.display();
           }
           else if (evBtnsData == ARM_BTN) {
             bombState = BombStates::ARMED;
@@ -199,109 +195,121 @@ void task() {
 
     case BombStates::ARMED: {
 
-        static uint8_t vecTryPassword[7];
-        const uint8_t vecTruePassword = {UP_BTN, UP_BTN, DOWN_BTN, DOWN_BTN, UP_BTN, DOWN_BTN, ARM_BTN};
-        const uint8_t vecLenght = 7;
+        const uint8_t vecLength = 7;
+        static uint8_t vecTryPassword[vecLength];
+        static uint8_t dataCounter = 0;
+        static uint8_t vecTruePassword[vecLength] = {UP_BTN, UP_BTN, DOWN_BTN, DOWN_BTN, UP_BTN, DOWN_BTN, ARM_BTN};
+        bool statePassword = false;
 
-        if (evBtns == true && vecLenght != 0) {
-          evBtns = false;
-          vecLenght--;
-          if (evBtnsData == UP_BTN) {
+        const uint32_t interval = 500;
+        static uint32_t previousMillis = 0;
+        static uint8_t ledState_BOMB_OUT = LOW;
+        uint32_t currentMillis = millis();
 
-          }
-          else if (evBtnsData == DOWN_BTN) {
+        enum class ArmedStates {COUNTER_PASSWORD, ABORT};
+        static ArmedStates armedState =  ArmedStates::COUNTER_PASSWORD;
 
-          }
-          else if (evBtnsData == ARM_BTN) {
+        switch (armedState) {
+          case ArmedStates::COUNTER_PASSWORD: {
 
-          }
-          else if (vecLenght <= 0) {
-            //limpiar el vector
-            //verificar si la contraseña es correcta
-            
-          }
+              //Cuenta regresiva
+
+              if (currentMillis - previousMillis >= interval) {
+                previousMillis = currentMillis;
+                if (ledState_BOMB_OUT == LOW) {
+                  ledState_BOMB_OUT = HIGH;
+                }
+                else {
+                  ledState_BOMB_OUT = LOW;
+                  counter--;
+                  display.clear();
+                  display.drawString(10, 20, String(counter));
+                  display.display();
+                }
+                digitalWrite(LED_COUNT, ledState_BOMB_OUT);
+                if (counter == 0) {
+                  digitalWrite(LED_COUNT, LOW);
+                  digitalWrite(BOMB_OUT, HIGH);
+                  display.clear();
+                  display.drawString(10, 20, "BOOM");
+                  display.display();
+                  delay(3000);
+                  digitalWrite(LED_COUNT, HIGH);
+                  digitalWrite(BOMB_OUT, LOW);
+                  counter = 20;
+                  display.clear();
+                  display.drawString(10, 20, String(counter));
+                  display.display();
+                  bombState = BombStates::CONFIG;
+                }
+              }
+
+              //ingrese la pasword
+
+
+              if (evBtns == true) {
+                evBtns = false;
+                if (dataCounter < vecLength) {
+                  if (evBtnsData == UP_BTN) {
+                    vecTryPassword[dataCounter] = evBtnsData;
+                  }
+                  else if (evBtnsData == DOWN_BTN) {
+                    vecTryPassword[dataCounter] = evBtnsData;
+                  }
+                  else if (evBtnsData == ARM_BTN) {
+                    vecTryPassword[dataCounter] = evBtnsData;
+                  }
+                  dataCounter++;
+                }
+              }
+              else if (dataCounter == vecLength) { //verifico que se el array este lleno y verificar si la contraseña es correcta y limpiar vector
+                disarmTask(vecTryPassword, vecTruePassword, vecLength, &statePassword); //statePassword va ser igual a la funcion que retorna un bool true si la clave es correcta
+                if (statePassword == true) { //si la clave es correcta entonces pase al estado CONFIG
+                  digitalWrite(LED_COUNT, HIGH);
+                  digitalWrite(BOMB_OUT, LOW);
+                  counter = 20;
+                  display.clear();
+                  display.drawString(10, 20, String(counter));
+                  display.display();
+                  for (uint8_t j = 0; j < vecLength; j++) {
+                    vecTryPassword[j] = 0;
+                  }
+                  bombState = BombStates::CONFIG;
+                }
+                else { //sino, i = 0, limpiar el vector y sigue la cuenta regresiva
+                  dataCounter = 0;
+                  for (uint8_t k = 0; k < vecLength; k++) {
+                    vecTryPassword[k] = 0;
+                  }
+                }
+              }
+
+
+            }
+            break;
+          case ArmedStates::ABORT: {
+
+            }
+            break;
+          default: {
+            }
+            break;
         }
-
-
-
-
-
-
-        /*const uint32_t interval = 200;
-          const uint32_t timeOut = 200;
-
-          static uint32_t previousMillis = 0;
-          static uint8_t ledState_BOMB_OUT = LOW;
-
-          uint32_t currentMillis = millis();
-          bool boomState = false;
-
-          if (currentMillis - previousMillis >= interval) {
-          previousMillis = currentMillis;
-          if (ledState_BOMB_OUT == LOW) {
-            ledState_BOMB_OUT = HIGH;
-          }
-          else {
-            ledState_BOMB_OUT = LOW;
-          }
-          digitalWrite(LED_COUNT, ledState_BOMB_OUT);
-          if (ledState_BOMB_OUT == HIGH) {
-            counter--;
-            display.clear();
-            display.drawString(10, 20, String(counter));
-            display.display();
-          }
-
-
-
-          if (counter == 0) {
-
-            currentMillis = millis();
-
-            digitalWrite(LED_COUNT, LOW);
-            digitalWrite(BOMB_OUT, HIGH);
-
-            display.clear();
-            display.drawString(10, 20, "BOOM");
-            display.display();
-          }
-          if (currentMillis - previousMillis >= timeOut) {
-            digitalWrite(LED_COUNT, HIGH);
-            digitalWrite(BOMB_OUT, LOW);
-
-            counter = 20;
-
-            display.clear();
-            display.drawString(10, 20, String(counter));
-            display.display();
-
-            bombState = BombStates::CONFIG;
-          }
-
-
-          }
-
-
-          }
-          else if (disarmTask(, , 7) == true) {
-          bombState = BombStates::CONFIG;
-          }
-          break;
-        */
       }
-
+      break;
     default: {
-
       }
       break;
   }
 }
 
-
-
-/*
-    bool disarmTask(, , uint8_t lenght) {
-      for (uint8_t i = 0; i < lengt, i++)
-
-      }
-*/
+void disarmTask(uint8_t *vecTryData, uint8_t *vecTrueData, uint8_t vecLengthData, bool *res) {
+  for (uint8_t i = 0; i < vecLengthData; i++) {
+    if (vecTryData[i] == vecTrueData[i]) {
+      *res = true;
+    }
+    else {
+      *res = false;
+    }
+  }
+}
